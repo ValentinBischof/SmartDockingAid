3610f06b9170046177934ec10b4a9402cacb1a7e
ValiZockt <55885298+ValiZockt@users.noreply.github.com>
2020-11-14 16:06:47 +0100

    Replace stock SAS module readout

---
+++ Source/Extensions/OverwriteModuleText.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SmartDockingAid.Extensions
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class OverwriteModuleText : MonoBehaviour
+    {
+        public void Start()
+        {
+            List<AvailablePart> parts = PartLoader.LoadedPartsList.Where(p => p.partPrefab.Modules.GetModule<ModuleSAS>()).ToList();
+
+            foreach (AvailablePart part in parts)
+            {
+                ModuleSAS SASmodule = part.partPrefab.FindModuleImplementing<ModuleSAS>();
+                AvailablePart.ModuleInfo moduleInfo = part.moduleInfos.Where(m => m.moduleName == "SAS").First();
+                int serviceLevel = (SASmodule.SASServiceLevel + 1);
+                string moduleText = string.Empty;
+                for (int i = 0; i < serviceLevel; i++)
+                    moduleText += $"{SASLevels[i]} \n";
+
+                if (part.partPrefab.HasModuleImplementing<ModuleDockingAid>())
+                    moduleText += $"{SASLevels[4]} \n";
+
+                moduleInfo.info = moduleText;
+            }
+        }
+
+        private string[] SASLevels = new string[5]
+        {
+            $"S0: Stability Assist",
+            $"S1: Prograde / Retrograde",
+            $"S2: Radial / Normal",
+            $"S3: Maneuver / Target",
+            $"S{AssetLoader.minPilotLevel}: Parallel+ / Parallel-"
+        };
+    }
+}
--- Source/Modules/ModuleDockingAid.cs
+++ Source/Modules/ModuleDockingAid.cs
@@ -1,15 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 // Modifies DockingAidAutopilot to be active if this PartModule is presence on the current vessel 
 
 namespace SmartDockingAid
 {
-    class ModuleDockingAid : PartModule
+    public class ModuleDockingAid : PartModule
     {
-        public override string GetInfo()
-        {
-            string info = $"This Probe Core has the abillity to set following additonal SAS Modes: \n Parallel - \n Parallel +";
-            return info;
-        }
-    }
+
+    } 
 }
